65125954767afedc86721337e0248436a882e214
Dulce Ramirez <dinggy.dot@gmail.com>
2023-10-12 23:15:47 -0600

    ya recibe la data de TMDB y esta en TS, se puede hacer la busqueda con input

--- src/components/OrderBy/SearchMovie.tsx
+++ src/components/OrderBy/SearchMovie.tsx
@@ -1,22 +1,52 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react';
+
+type Movie = {
+  id:number;
+  title: string;
+  release_date: string;
+  poster_path: string;
+  overview: string;
+};
 
 export const SearchMovie: React.FC = () => {
-  const [busqueda, setBusqueda] = useState('')
+  const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
+  const API_KEY = '03d8479e6ac8e870c3ef0fea7b1b15c3';
+
+  const [busqueda, setBusqueda] = useState<string>('');
+  const [peliculas, setPeliculas] = useState<Movie[]>([]);
+
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setBusqueda(e.target.value)
-  }
+    setBusqueda(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    fetchMovies();
+  };
+
+  const fetchMovies = async () => {
+    try {
+      const response = await fetch(`${BASE_URL}?query=${busqueda}&api_key=${API_KEY}`);
+      const data = await response.json();
+      setPeliculas(data);
+    } catch (error) {
+      console.error("Error fetching movies", error);
+    }
+  };
+
   return (
     <div className='container'>
-        <form>
-            <input
-            type='text'
-            placeholder='Search by Name'
-            value={busqueda}
-            onChange={handleInputChange}
-            />
-            <button type='submit' className='search-button'></button>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Search by Name'
+          value={busqueda}
+          onChange={handleInputChange}
+        />
+        <button type='submit' className='search-button'>
+          Search
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
