64eda872d59cd47ba8f8f7d0daa5c30f39be9eb1
Dulce Ramirez <dinggy.dot@gmail.com>
2023-11-03 18:04:35 -0600

    Preparación para migrar a vite

--- src/components/MovieDetail/MovieDetail.tsx
+++ src/components/MovieDetail/MovieDetail.tsx
@@ -10,18 +10,30 @@ export const MovieDetail: React.FC<MovieDetailProps> = ({
   movie,
   onReturn,
 }) => {
+  const defaultImage = "/gato-jugando.png";
   return (
     <>
       <h2 className="movie-title">{movie.title}</h2>
       <button className="detail-button" onClick={onReturn}>Return to List</button>
       <div className="Movie-detail-container">
-        <section>
+      <section>
+      {movie.poster_path ? (
+        <img
+          src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+          alt={movie.title}
+          className="movie-poster"
+        />
+      ) : (
+        <>
           <img
-            src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
-            alt={movie.title}
-            className="movie-poster"
+            src={defaultImage}
+            alt="Default movie poster"
+            className="movie-poster-no-image"
           />
-        </section>
+          <div className="no-image-text">No Image Available</div>
+        </>
+      )}
+    </section>
         <aside className="Movie-text">
           <p>{movie.release_date}</p>
           <p>{movie.genres.map((genre: any) => genre.name).join(", ")}</p>
